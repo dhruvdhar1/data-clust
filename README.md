e0cd2fca08c62996c6b631d6669c801ee8764818
Dhruv Dhar <dhruvdhar1@gmail.com>
2023-08-22 22:46:29 -0400

    added DBScan algorithm to library

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ This GitHub repository is a go-to resource for a collection of open-source unsup
 
 ### KMeans
 
-The below code block demonstrates usage using the MNIST dataset
+The below code block demonstrates usage using the MNIST dataset:
 
 ```python
 from keras.datasets import mnist
@@ -39,3 +39,61 @@ testX = testX.astype('float32') / 255
 
 labels, centroids = kmeans(trainX, 10)
 ```
+
+### Gaussian Mixture model
+
+The below code block demonstrates usage:
+```python
+import numpy as np
+import matplotlib.pyplot as plt
+from dataclust import GaussianMixtureModel
+
+inputFile = open("./data/2gaussian.txt", 'r')
+num_lines = len(inputFile.readlines())
+
+dataset = np.zeros((num_lines, 2))
+inputFile = open("./data/2gaussian.txt", 'r')
+ind = 0
+for line in inputFile:
+    points = line.strip().split(" ")
+    x = points[0]
+    y = points[1]
+    dataset[ind][0] = x
+    dataset[ind][1] = y
+    ind += 1
+
+gmm = GaussianMixtureModel(k=2)
+mean, cov, pi = gmm.fit_transform(dataset)
+```
+
+### DB Scan
+The below code block demonstrates usage using a custom dataset:
+```python
+import numpy as np
+import pandas as pd
+from dataclust import dbscan
+import matplotlib.pyplot as plt
+
+data_circle = pd.read_csv(r'data/circle.csv')
+df_circle = pd.DataFrame(data_circle, columns=['Xcircle_X1', 'Xcircle_X2'])
+df_circle.rename(columns={'Xcircle_X1': 'x', 'Xcircle_X2':'y'}, inplace = True)
+
+EPSILON = 0.1
+MIN_PTS = 5
+
+clustered = dbscan(EPSILON, MIN_PTS, df_circle)
+
+idx , cluster = list(zip(*clustered))
+cluster_df = pd.DataFrame(clustered, columns = ["ind", "cluster"])
+raw_circle_data = df_circle.to_numpy()
+
+for clust in np.unique(cluster):
+    plt.scatter(raw_circle_data[cluster_df["ind"][cluster_df["cluster"] == clust].values, 0], raw_circle_data[cluster_df["ind"][cluster_df["cluster"] == clust].values, 1], s=10, label=f"Cluster{clust}")
+
+plt.legend([f"Cluster {clust}" for clust in np.unique(cluster)], loc ="lower right")
+plt.title('Clustered Data')
+plt.xlabel('X')
+plt.ylabel('Y')
+```
+This is how the clustered data looks after running DBScan
+![alt text](./res/dbscan-circles.png)
--- dataclust.py
+++ dataclust.py
@@ -2,6 +2,7 @@
 Author: Dhruv Dhar
 email: dhruvdhar1@gmail.com
 """
+import random
 import numpy as np
 from scipy.stats import multivariate_normal
 
@@ -42,6 +43,68 @@ def kmeans(X, num_clusters, max_iterations = 100):
 
     return labels, centroids
 
+def check_core_point(eps,minPts, df, index):
+    x, y = df.iloc[index]['x']  ,  df.iloc[index]['y']
+    temp =  df[((np.abs(x - df['x']) <= eps) & (np.abs(y - df['y']) <= eps)) & (df.index != index)]
+    
+    if len(temp) >= minPts:
+        return (temp.index , 1)
+    
+    elif (len(temp) < minPts) and len(temp) > 0:
+        return (temp.index , 2)
+    
+    elif len(temp) == 0:
+        return (temp.index , 3)
+
+def dbscan(eps, minPts, df):
+    #initiating cluster number
+    cluster_num = 0
+
+    q = set()
+    unvisited = list(df.index)
+    clusters = []
+    
+    while (len(unvisited) > 0): #run until all points have been visited
+
+        #identifier for first point of a cluster
+        first_point = True
+        
+        #choose a random unvisited point
+        q.add(random.choice(unvisited))
+        
+        while len(q) > 0:
+            pop = q.pop()
+            unvisited.remove(pop)
+            neighbor_ind, point_type = check_core_point(eps, minPts, df, pop)
+            
+            #dealing with an edge case
+            if point_type == 2 and first_point:
+                
+                clusters.append((pop, -1))
+                for ind in neighbor_ind:
+                    clusters.append((ind, -1))
+                unvisited = [element for element in unvisited if element not in neighbor_ind]
+                continue
+            first_point = False
+            
+            #CORE POINT
+            if point_type == 1:
+                clusters.append((pop,cluster_num))
+                neighbor_ind = set(neighbor_ind) & set(unvisited)
+                q.update(neighbor_ind)
+
+            #BORDER POINT
+            elif point_type == 2:
+                clusters.append((pop,cluster_num))
+            
+            #OUTLIER
+            elif point_type == 3:
+                clusters.append((pop, -1))
+                
+        if not first_point:
+            cluster_num += 1
+        
+    return clusters
 
 class GaussianMixtureModel:
 
